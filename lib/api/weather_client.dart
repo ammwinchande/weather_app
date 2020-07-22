0e782e1c0fb99085f1f383b79bcb08d5f1c76443
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-07-22 20:47:58 +0300

    added api fetch configs

---
+++ lib/api/weather_client.dart
@@ -0,0 +1,6 @@
+import 'package:weatloc/config/api_key.dart';
+
+class WeatherClient {
+  static final String apiKey = APIKey.key;
+  static final String host = 'http://api.openweathermap.org/';
+}
---
+++ lib/config/api_key.dart
@@ -0,0 +1,3 @@
+class APIKey {
+  static const String key = 'afa87218f1391db925d59dc04789ad53';
+}
